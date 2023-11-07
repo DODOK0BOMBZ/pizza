a92da281e892205fcce84b10a43b55411f5c5dbd
DODOK0BOMBZ <138174459+DODOK0BOMBZ@users.noreply.github.com>
2023-11-07 16:46:29 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+pizzaListArray = [];
+
+function getmenu()
+	{    
+        
+        var item = document.getElementById("pizzaName").value; 
+       
+        
+        pizzaListArray.push(item);
+        
+        var removeCommas = pizzaListArray.join("<br># ");
+        document.getElementById("displayMenu").innerHTML =  "# " + removeCommas;
+	}
